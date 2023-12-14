a138671ec2ee1f2fba30e67a7ea8cc60ee20da08
Leandro Schillreff <leandroschillreff@hotmail.com>
2023-12-14 17:36:54 -0400

    feat: browser router

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { App } from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { App } from './App';
+import { AppProvider } from './contexts';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
