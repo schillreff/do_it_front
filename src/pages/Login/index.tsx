c155e17768e4ef513991a84d661c2d2a7587362d
Leandro Schillreff <leandroschillreff@hotmail.com>
2023-12-17 12:23:29 -0400

    feat: added yup and responsiveness

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Button,
   Flex,
   Grid,
@@ -8,6 +9,7 @@ import {
   VStack,
 } from '@chakra-ui/react';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { FaEnvelope, FaLock } from 'react-icons/fa';
 import * as yup from 'yup';
@@ -25,6 +27,8 @@ interface SignInData {
 }
 
 export const Login = () => {
+  const [loading, setLoading] = useState(false);
+
   const {
     formState: { errors },
     register,
@@ -37,47 +41,68 @@ export const Login = () => {
 
   return (
     <Flex
-      padding='10px 15px'
+      padding={['10px 15px', '10px 15px', '0px', '0px']}
       alignItems='center'
-      height='100vh'
-      bgGradient='linear(to-r, purple.800 65%, white 35%)'
+      justifyContent={'center'}
+      height={['auto', 'auto', '100vh', '100vh']}
+      bgGradient={[
+        'linear(to-b, purple.800 65%, white 35%)',
+        'linear(to-b, purple.800 65%, white 35%)',
+        'linear(to-r, purple.800 65%, white 35%)',
+        'linear(to-r, purple.800 65%, white 35%)',
+      ]}
       color='white'
     >
       <Flex
-        w={'100%'}
+        w={['100%', '100%', '90%', '65%']}
         justifyContent={'center'}
-        flexDirection={'row'}
+        flexDirection={['column', 'column', 'row', 'row']}
         alignItems={'center'}
       >
-        <Grid w={'100%'} paddingRight={'100px'}>
-          <Image src={LogoPrimary} alt='doit' boxSize={'120px'} />
-          <Heading as={'h1'}> O jeito fácil, grátis</Heading>
-          <Text>
+        <Grid w={['100%', '100%', '50%', '50%']} >
+          <Image
+            src={LogoPrimary}
+            alt='doit'
+            boxSize={['120px', '120px', '150px', '150px']}
+          />
+          <Heading mt={'4'} as={'h1'}>
+            {' '}
+            O jeito fácil, grátis
+          </Heading>
+          <Text maxW={'350px'}>
             Flexível e atrativo de gerenciar{' '}
-            <b>seus projetos em uma única plataforma</b>
+            <b> seus projetos em uma única plataforma</b>
           </Text>
         </Grid>
         <Grid
           onSubmit={handleSubmit(handleSignIn)}
           as={'form'}
-          mt={'4'}
-          w={'50%'}
+          w={['100%', '100%', '40%', '40%']}
+          mt={['4', '4', '0', '0']}
           padding={'30px 15px'}
           border={'3px solid'}
           borderColor={'gray.100'}
+          borderRadius={'8px'}
           bg={'white'}
           color={'gray.900'}
         >
           <Heading size={'lg'}>Bem vindo de volta!</Heading>
           <VStack mt={'6'} spacing={'5'}>
-            <Input
-              placeholder={'Digite seu login'}
-              icon={FaEnvelope}
-              label='Login'
-              type='email'
-              error={errors.email}
-              name='email'
-            />
+            <Box w={'100%'}>
+              <Input
+                placeholder={'Digite seu login'}
+                icon={FaEnvelope}
+                label='Email'
+                type='email'
+                error={errors.email}
+                {...register('email')}
+              />
+              {!errors.email && (
+                <Text ml={'1'} mt={'1'} color={'gray.300'}>
+                  Exemplo: nome@email.com
+                </Text>
+              )}
+            </Box>
             <Input
               placeholder={'Digite sua senha'}
               icon={FaLock}
@@ -87,7 +112,32 @@ export const Login = () => {
               {...register('password')}
             />
           </VStack>
-          <Button type='submit'>Entrar</Button>
+          <VStack mt={'4'} spacing={'5'}>
+            <Button
+              isLoading={loading}
+              bg={'purple.800'}
+              w={'100%'}
+              color={'white'}
+              h={'60px'}
+              borderRadius={'8px'}
+              _hover={{ background: 'purple.900' }}
+              type='submit'
+            >
+              Entrar
+            </Button>
+            <Text color={'gray.400'}> Ainda não possui uma conta?</Text>
+            <Button
+              bg={'gray.100'}
+              w={'100%'}
+              color={'gray.300'}
+              h={'60px'}
+              borderRadius={'8px'}
+              _hover={{ background: 'gray.200' }}
+              type='submit'
+            >
+              Cadastrar
+            </Button>
+          </VStack>
         </Grid>
       </Flex>
     </Flex>
