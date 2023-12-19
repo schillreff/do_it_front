0eaf211dafeec547d66ccb626b0f2c6e0619c02f
Leandro Schillreff <leandroschillreff@hotmail.com>
2023-12-19 12:48:25 -0400

    feat: signup page

---
+++ src/pages/Signup/SignupForm.tsx
@@ -0,0 +1,97 @@
+import { Box, Button, Grid, Heading, Text, VStack } from '@chakra-ui/react';
+import {
+  DeepMap,
+  FieldError,
+  FieldValues,
+  UseFormRegister,
+} from 'react-hook-form';
+import { FaEnvelope, FaLock, FaUser } from 'react-icons/fa';
+import { Input } from '../../components/Form/input';
+
+interface SignUpProps {
+  handleSignUp: () => void;
+  errors: DeepMap<FieldValues, FieldError>;
+  register: UseFormRegister<{
+    name: string;
+    email: string;
+    password: string;
+    confirm_password: string;
+  }>;
+  loading: boolean;
+}
+
+export const SignupForm = ({
+  handleSignUp,
+  errors,
+  register,
+  loading,
+}: SignUpProps) => (
+  <Grid
+    onSubmit={handleSignUp}
+    as={'form'}
+    w={['100%', '320px', '350px', '400px']}
+    mt={['4', '4', '0', '0']}
+    padding={'30px 25px'}
+    border={'3px solid'}
+    borderColor={'gray.100'}
+    borderRadius={'8px'}
+    bg={'white'}
+    color={'gray.900'}
+  >
+    <Heading size={'lg'}>Crie sua conta</Heading>
+    <VStack mt={'6'} spacing={'5'}>
+      <Input
+        placeholder={'Digite seu nome'}
+        icon={FaUser}
+        label='Nome'
+        error={errors.name}
+        {...register('name')}
+      />
+      <Box w={'100%'}>
+        <Input
+          placeholder={'Digite seu login'}
+          icon={FaEnvelope}
+          label='Email'
+          type='email'
+          error={errors.email}
+          {...register('email')}
+        />
+        {!errors.email && (
+          <Text ml={'1'} mt={'1'} color={'gray.300'}>
+            Exemplo: nome@email.com
+          </Text>
+        )}
+      </Box>
+      <Input
+        placeholder={'Digite sua senha'}
+        icon={FaLock}
+        label='Senha'
+        type='password'
+        error={errors.password}
+        {...register('password')}
+      />
+      <Input
+        placeholder={'Confirme sua senha'}
+        icon={FaLock}
+        label='Confirmaçao de senha'
+        type='password'
+        error={errors.confirm_password}
+        {...register('confirm_password')}
+      />
+    </VStack>
+
+    <Button
+      w={'100%'}
+      mt={'4'}
+      isLoading={loading}
+      bg={'purple.800'}
+      color={'white'}
+      h={'60px'}
+      borderRadius={'8px'}
+      _hover={{ background: 'purple.900' }}
+      type='submit'
+    >
+      Finalizar Cadastro
+    </Button>
+  </Grid>
+);
---
+++ src/pages/Signup/index.tsx
@@ -0,0 +1,70 @@
+import { Flex } from '@chakra-ui/react';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { SignupForm } from './SignupForm';
+import { SignupInfo } from './SignupInfo';
+
+const signUpSchema = yup.object().shape({
+  name: yup.string().required('Nome obrigatório'),
+  email: yup.string().required('Email obrigatório').email('Email inválido'),
+  password: yup.string().required('Senha obrigatória'),
+  confirm_password: yup
+    .string()
+    .required('Confirmação de senha obrigatória')
+    .oneOf([yup.ref('password')], 'Senhas diferentes'),
+});
+
+interface SignUpData {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export const SignUp = () => {
+  const [loading, setLoading] = useState(false);
+
+  const {
+    formState: { errors },
+    register,
+    handleSubmit,
+  } = useForm({
+    resolver: yupResolver(signUpSchema),
+  });
+
+  const handleSignUp = (data: SignUpData) => {
+    console.log(data);
+  };
+
+  return (
+    <Flex
+      padding={['10px 15px', '10px 15px', '0px', '0px']}
+      alignItems='center'
+      justifyContent={'center'}
+      height={['auto', 'auto', '100vh', '100vh']}
+      bgGradient={[
+        'linear(to-b, purple.800 65%, white 35%)',
+        'linear(to-b, purple.800 65%, white 35%)',
+        'linear(to-l, purple.800 65%, white 35%)',
+        'linear(to-l, purple.800 65%, white 35%)',
+      ]}
+      color='white'
+    >
+      <Flex
+        w={['100%', '100%', '90%', '65%']}
+        justifyContent={'center'}
+        flexDirection={['column', 'column', 'row', 'row']}
+        alignItems={'center'}
+      >
+        <SignupForm
+          handleSignUp={handleSubmit(handleSignUp)}
+          errors={errors}
+          register={register}
+          loading={loading}
+        />
+        <SignupInfo />
+      </Flex>
+    </Flex>
+  );
+};
