972e7f4aa4672fb176ab4c805d4c227b4d70aed8
Leandro Schillreff <leandroschillreff@hotmail.com>
2023-12-23 15:30:08 -0400

    feat: go back button

--- src/pages/Signup/GoBackButton.tsx
+++ src/pages/Signup/GoBackButton.tsx
@@ -2,6 +2,6 @@ import { Center, Text } from '@chakra-ui/react';
 
 export const GoBackButton = () => {
   <Center as='button'>
-    <Text>Button voltar</Text>
+    <Text>Voltar</Text>
   </Center>;
 };
