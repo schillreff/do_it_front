a6de1b5c89675b84b18344454403d0fc875d3831
Leandro Schillreff <leandroschillreff@hotmail.com>
2024-01-16 16:31:21 -0400

    feat: modal error

--- src/components/Modal/ModalError.tsx
+++ src/components/Modal/ModalError.tsx
@@ -1,5 +1,6 @@
 import {
   Button,
+  Center,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -21,26 +22,41 @@ interface ModalErrorProps {
 export const ModalError = ({ isOpen, onClose, error }: ModalErrorProps) => (
   <Modal isOpen={isOpen} onClose={onClose}>
     <ModalOverlay />
-    <ModalContent>
-      <ModalHeader>
-        <FaExclamation color={theme.colors.red['500']} />
-        Oops!
+    <ModalContent color={'gray.800'}>
+      <ModalHeader display={'flex'}>
+        <Center bg={'red.500'} w={'30px'} h={'30px'} borderRadius={'5px'}>
+          <FaExclamation color={theme.colors.white} />
+        </Center>
+        <Text fontWeight={'bold'} ml={'2'}>
+          Oops!
+        </Text>
       </ModalHeader>
-      <ModalCloseButton />
-      <ModalBody>
-        <Text> Ocorreu algum erro! {error} </Text>
+      <ModalCloseButton
+        bg={'red.500'}
+        color={'white'}
+        _hover={{ bg: 'red.600' }}
+      />
+      <ModalBody color={'gray.400'} textAlign={'center'}>
+        <Text>
+          {' '}
+          Ocorreu algum erro! <b>{error}</b>{' '}
+        </Text>
       </ModalBody>
-
-      <ModalFooter>
+      <ModalFooter display={'column'}>
         <Button
           bg={'red.500'}
           color={'white'}
           w={'100%'}
+          h={'60px'}
           _hover={{ bg: 'red.600' }}
           onClick={onClose}
         >
           Tentar novamente
         </Button>
+        <Text mt={'4'} textAlign={'center'}>
+          Você já pode tentar novamente, <b>clicando</b> no botão acima ou
+          aguarde alguns minutos ...
+        </Text>
       </ModalFooter>
     </ModalContent>
   </Modal>
