627a0dece4196abcf911857c3214487823997f95
Leandro Schillreff <leandroschillreff@hotmail.com>
2024-01-17 16:04:55 -0400

    feat: modal success

--- src/components/Modal/ModalSuccess.tsx
+++ src/components/Modal/ModalSuccess.tsx
@@ -1,35 +1,86 @@
 import {
+  Box,
   Button,
+  Center,
   Modal,
   ModalBody,
-  ModalCloseButton,
   ModalContent,
   ModalFooter,
   ModalHeader,
   ModalOverlay,
   Text,
 } from '@chakra-ui/react';
+import { FaExclamation, FaTimes } from 'react-icons/fa';
+import { theme } from '../../styles/theme';
 
 interface ModalSuccessProps {
   isOpen: boolean;
   onClose: () => void;
+  message: string;
+  buttonMMessage: string;
+  onClick: () => void;
+  secondaryText: string;
 }
 
-export const ModalSuccess = ({ isOpen, onClose }: ModalSuccessProps) => (
+export const ModalSuccess = ({
+  isOpen,
+  onClose,
+  message,
+  buttonMMessage,
+  onClick,
+  secondaryText,
+}: ModalSuccessProps) => (
   <Modal isOpen={isOpen} onClose={onClose}>
     <ModalOverlay />
-    <ModalContent>
-      <ModalHeader>Yeesss...</ModalHeader>
-      <ModalCloseButton />
-      <ModalBody>
-        <Text> Success </Text>
+    <ModalContent padding={'2'} bg={'white'} color={'gray.800'}>
+      <ModalHeader display={'flex'}>
+        <Center bg={'purple.500'} w={'30px'} h={'30px'} borderRadius={'5px'}>
+          <FaExclamation color={theme.colors.white} />
+        </Center>
+        <Text fontWeight={'bold'} ml={'2'}>
+          Yeesss...
+        </Text>
+        <Center
+          onClick={onClose}
+          as={'button'}
+          ml={'auto'}
+          w={'30px'}
+          h={'30px'}
+          bg={'red.500'}
+          fontSize={'lg'}
+          borderRadius={'md'}
+        >
+          <FaTimes color={theme.colors.white} />
+        </Center>
+      </ModalHeader>
+      <ModalBody textAlign={'center'}>
+        <Text>
+          {' '}
+          <Box
+            dangerouslySetInnerHTML={{
+              __html: message,
+            }}
+          ></Box>
+        </Text>
       </ModalBody>
-
-      <ModalFooter>
-        <Button colorScheme='blue' mr={3} onClick={onClose}>
-          Close
+      <ModalFooter flexDirection={'column'}>
+        <Button
+          bg={'purple.500'}
+          color={'white'}
+          w={'100%'}
+          onClick={onClick}
+          _hover={{ bg: 'purple.600' }}
+          h={'60px'}
+        >
+          {buttonMMessage}
         </Button>
-        <Button variant='ghost'>Secondary Action</Button>
+        <Text mt={'4'} align={'center'}>
+          <Box
+            dangerouslySetInnerHTML={{
+              __html: secondaryText,
+            }}
+          ></Box>
+        </Text>
       </ModalFooter>
     </ModalContent>
   </Modal>
