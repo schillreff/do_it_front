a7d4eae54577e0ea36b153efaad6ab5652d06957
Leandro Schillreff <leandroschillreff@hotmail.com>
2024-02-19 17:52:53 -0400

    feat: text area

---
+++ src/components/Form/TextArea.tsx
@@ -0,0 +1,101 @@
+import {
+  Textarea as ChakraTextArea,
+  TextareaProps as ChakraTextAreaProps,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  InputGroup,
+  InputLeftElement,
+} from '@chakra-ui/react';
+import {
+  ForwardRefRenderFunction,
+  forwardRef,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react';
+import { FieldError } from 'react-hook-form';
+import { IconType } from 'react-icons';
+
+interface InputProps extends ChakraTextAreaProps {
+  name: string;
+  label?: string;
+  error?: FieldError | null;
+  icon?: IconType;
+}
+
+type inputVariationOptions = {
+  [key: string]: string;
+};
+
+const inputVariation: inputVariationOptions = {
+  error: 'red.500',
+  default: 'gray.200',
+  focus: 'purple.800',
+  filled: 'green.500',
+};
+
+const TextareaBase: ForwardRefRenderFunction<HTMLTextAreaElement, InputProps> = (
+  { name, error = null, icon: Icon, label, ...rest },
+  ref,
+) => {
+  const [variation, setVariation] = useState('default');
+  const [value, setValue] = useState('');
+
+  useEffect(() => {
+    if (error) {
+      return setVariation('error');
+    }
+  }, [error]);
+
+  const handleInputFocus = useCallback(() => {
+    if (!error) {
+      return setVariation('focus');
+    }
+  }, [error]);
+
+  const handleInputBlur = useCallback(() => {
+    if (value.length > 1 && !error) {
+      return setVariation('filled');
+    }
+  }, [error, value]);
+
+  return (
+    <FormControl isInvalid={!!error}>
+      {!!label && <FormLabel color={'gray.400'}>{label}</FormLabel>}
+
+      <InputGroup flexDirection={'column'}>
+        {Icon && (
+          <InputLeftElement color={inputVariation[variation]} mt={'2.5'}>
+            <Icon />
+          </InputLeftElement>
+        )}
+
+        <ChakraTextArea
+          id={name}
+          name={name}
+          onChangeCapture={(event) => setValue(event.currentTarget.value)}
+          onBlurCapture={handleInputBlur}
+          onFocus={handleInputFocus}
+          borderColor={inputVariation[variation]}
+          color={inputVariation[variation]}
+          bg={'gray.50'}
+          variant={'outline'}
+          _hover={{ bgColor: 'gray.100' }}
+          _placeholder={{ color: 'gray.400' }}
+          _focus={{
+            bg: 'gray.100',
+          }}
+          size={'lg'}
+          h={'60px'}
+          ref={ref}
+          {...rest}
+        />
+
+        {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
+      </InputGroup>
+    </FormControl>
+  );
+};
+
+export const TextArea = forwardRef(TextareaBase);
